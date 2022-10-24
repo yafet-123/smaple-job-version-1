481e308023fa80e75a4ffe914f050d25d0627350
yafet-123 <yafetaddisu123@gmail.com>
2022-10-24 17:59:04 +0300

    adding advice and hero

--- components/AdviceAndNews.js
+++ components/AdviceAndNews.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export function AdviceAndNews() {
   return (
@@ -11,6 +12,22 @@ export function AdviceAndNews() {
         Make the most out of your job search and cut your time as job seeker
         short.
       </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <div className="flex items-center justify-center">
+          <Image src="/images/jobSearch.jpg" width={75} height={75} />
+          <h1>job search tips</h1>
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/jobInterview.jpg" width={75} height={75} />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/cv.jpg" width={75} height={75} />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/Careeradvice.jpg" width={75} height={75} />
+        </div>
+      </div>
     </div>
   );
 }
