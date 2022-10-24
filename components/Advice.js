c91b5d7141a54c1cc3c0851fc31ed37dea42d51b
yafet-123 <yafetaddisu123@gmail.com>
2022-10-25 01:52:23 +0300

    adding advice

--- components/Advice.js
+++ components/Advice.js
@@ -6,7 +6,7 @@ export function Advice() {
   return (
     <div className="bg-white w-full h-[40rem] p-20">
       <h1 className="text-center w-full uppercase text-[2.75rem] font-bold">
-        carrer advice & news
+        carrer advice
       </h1>
       <p className="text-center text-2xl font-light my-5">
         Make the most out of your job search and cut your time as job seeker
