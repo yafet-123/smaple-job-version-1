454299e6cef58f9b1b619af9736e198cbf87f0e7
yafet-123 <yafetaddisu123@gmail.com>
2022-10-25 02:35:11 +0300

    adding news

--- components/News.js
+++ components/News.js
@@ -1,53 +1,39 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export function News() {
   return (
-    <div className="bg-white w-full h-[40rem] p-20">
-      <h1 className="text-center w-full uppercase text-[2.75rem] font-bold">
-        carrer advice
-      </h1>
-      <p className="text-center text-2xl font-light my-5">
-        Make the most out of your job search and cut your time as job seeker
-        short.
-      </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        <Link href="/">
-          <a className="flex flex-col items-center justify-center">
-            <div className="border border-slate-300 rounded-xl p-5 mb-10">
-              <Image src="/images/jobSearch.jpg" width={75} height={75} />
-            </div>
-
-            <h1 className="text-3xl text-blue-800 capitalize">
-              job search tips
+    <div className="bg-white w-full h-screen p-20 grid grid-cols-1 md:grid-cols-2 gap-5 mb-10">
+      <div className="news-background w-full h-screen"></div>
+      <div className="flex flex-col">
+        <h1 className="text-center w-full uppercase text-7xl font-medium my-10">
+          job news
+        </h1>
+        <div className="flex">
+          <Image
+            src="/images/bgImage1.avif"
+            width="1000px"
+            height="1000px"
+            className="border rounded-xl"
+          />
+          <div className="flex flex-col justify-between ml-10">
+            <h1 className="text-left w-full uppercase text-3xl font-medium">
+              Planning
             </h1>
-          </a>
-        </Link>
-        <Link href="/">
-          <a className="flex flex-col items-center justify-center">
-            <div className="border border-slate-300 rounded-xl p-5 mb-10">
-              <Image src="/images/jobInterview.jpg" width={75} height={75} />
-            </div>
-            <h1 className="text-3xl text-blue-800 capitalize">
-              job interview tips
-            </h1>
-          </a>
-        </Link>
-        <Link href="/">
-          <a className="flex flex-col items-center justify-center">
-            <div className="border border-slate-300 rounded-xl p-5 mb-10">
-              <Image src="/images/cv.jpg" width={75} height={75} />
-            </div>
-            <h1 className="text-3xl text-blue-800 capitalize">cv tips</h1>
-          </a>
-        </Link>
-        <Link href="/">
-          <a className="flex flex-col items-center justify-center">
-            <div className="border border-slate-300 rounded-xl p-5 mb-10">
-              <Image src="/images/Careeradvice.jpg" width={75} height={75} />
-            </div>
-            <h1 className="text-3xl text-blue-800 capitalize">carrer Advice</h1>
-          </a>
-        </Link>
+            <p className="text-left w-full capitalize text-lg font-normal">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tempor finibus ligula, in consectetur augue semper a. Vivamus ut
+              ante sagittis, pellentesque quam sed, interdum lacus. Orci varius
+              natoque penatibus et magnis dis parturient montes, nascetur
+              ridiculus mus. Aenean hendrerit nulla augue, non rhoncus nisi
+              imperdiet quis.
+            </p>
+            <button className="text-center capitalize text-lg font-bold p-5 bg-blue-700 hover:bg-gray-200 border rounded-xl w-1/2 text-white hover:text-blue-700">
+              More
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
