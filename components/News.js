f9a312d3eeb5b1735a15fefa4665cd2f6028c37d
yafet-123 <yafetaddisu123@gmail.com>
2022-10-25 15:39:07 +0300

    adding news

--- components/News.js
+++ components/News.js
@@ -10,12 +10,29 @@ export function News() {
         <h1 className="text-center w-full uppercase text-7xl font-medium my-10">
           job news
         </h1>
-        <div className="flex flex-col md:flex-row w-full h-72 mb-5">
+        <div className="flex flex-col md:flex-row w-full h-72 mt-20">
           <img
             src="/images/bgImage1.avif"
-            width="200px"
-            height="100%"
-            className="border rounded-xl"
+            className="border rounded-xl w-full md:w-96"
+          />
+          <div className="flex flex-col justify-between ml-0 md:ml-10 mt-5 md:mt-0">
+            <h1 className="text-left w-full uppercase text-3xl font-medium">
+              Planning
+            </h1>
+            <p className="text-left capitalize text-lg font-normal leading-9 w-full md:w-3/4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tempor finibus ligula, in consectetur augue semper.
+            </p>
+            <button className="text-center capitalize text-lg font-bold p-5 bg-blue-700 hover:bg-gray-200 border rounded-xl w-1/2 text-white hover:text-blue-700">
+              More
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row w-full h-72 mt-20">
+          <img
+            src="/images/bgImage1.avif"
+            className="border rounded-xl w-full md:w-96"
           />
           <div className="flex flex-col justify-between ml-0 md:ml-10 mt-5 md:mt-0">
             <h1 className="text-left w-full uppercase text-3xl font-medium">
@@ -31,33 +48,10 @@ export function News() {
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row w-full h-72 mb-5">
+        <div className="flex flex-col md:flex-row w-full h-72 mt-20">
           <img
             src="/images/bgImage1.avif"
-            width="200px"
-            height="100%"
-            className="border rounded-xl"
-          />
-          <div className="flex flex-col justify-between ml-0 md:ml-10 mt-5 md:mt-0">
-            <h1 className="text-left w-full uppercase text-3xl font-medium">
-              Planning
-            </h1>
-            <p className="text-left capitalize text-lg font-normal leading-9 w-full md:w-3/4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              tempor finibus ligula, in consectetur augue semper a.
-            </p>
-            <button className="text-center capitalize text-lg font-bold p-5 bg-blue-700 hover:bg-gray-200 border rounded-xl w-1/2 text-white hover:text-blue-700">
-              More
-            </button>
-          </div>
-        </div>
-
-        <div className="flex flex-col md:flex-row w-full h-72 mb-5">
-          <img
-            src="/images/bgImage1.avif"
-            width="200px"
-            height="100%"
-            className="border rounded-xl"
+            className="border rounded-xl w-full md:w-96 h-full"
           />
           <div className="flex flex-col justify-between ml-0 md:ml-10 mt-5 md:mt-0">
             <h1 className="text-left w-full uppercase text-3xl font-medium">
