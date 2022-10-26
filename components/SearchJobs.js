942181d21590e8fea1f2d8ddad6de05619cc2644
yafet-123 <yafetaddisu123@gmail.com>
2022-10-27 02:16:15 +0300

    editing search jobs

--- components/SearchJobs.js
+++ components/SearchJobs.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { LatestJobsList } from "../data/LatestJobs";
 export function SearchJobs() {
   return (
     <div className="flex flex-col w-full h-screen py-20 px-32">
@@ -7,7 +7,14 @@ export function SearchJobs() {
         Search and Find Jobs in Ethiopia
       </h1>
       <hr className="w-full bg-gray-200 mb-5" />
-      <div className="w-full h-[55rem] border rounded-xl border-slate-800 shadow-2xl shadow-sky-200"></div>
+      <div className="w-full h-[55rem] border rounded-xl border-slate-800 shadow-2xl shadow-sky-200">
+        {LatestJobsList.map((index, data) => (
+          <div className="grid grid-cols-3 gap-5 px-20 py-5">
+            <h1>Accounting finance</h1>
+            <h1>89</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
