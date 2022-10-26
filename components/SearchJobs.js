77eac0f23a27761589858a48c492257c6f7145ed
yafet-123 <yafetaddisu123@gmail.com>
2022-10-27 02:29:57 +0300

    editing search jobs

--- components/SearchJobs.js
+++ components/SearchJobs.js
@@ -7,13 +7,19 @@ export function SearchJobs() {
         Search and Find Jobs in Ethiopia
       </h1>
       <hr className="w-full bg-gray-200 mb-5" />
-      <div className="w-full h-[55rem] border rounded-xl border-slate-800 shadow-2xl shadow-sky-200">
-        {LatestJobsList.map((index, data) => (
-          <div className="grid grid-cols-3 gap-5 px-20 py-5">
-            <h1>Accounting finance</h1>
-            <h1>89</h1>
-          </div>
-        ))}
+      <div className="w-full h-[55rem] border rounded-xl border-slate-800 shadow-2xl shadow-sky-200 overflow-y-scroll">
+        <div className="grid md:grid-cols-3 gap-5 px-20 py-10">
+          {LatestJobsList.map((index, data) => (
+            <div className="flex justify-between items-center mb-5">
+              <h1 className="font-normal text-sm md:text-lg lg:text-xl capitalize">
+                Accounting finance
+              </h1>
+              <h1 className="px-5 py-2 border rounded-xl border-gray-200 text-blue-800 font-bold text-sm md:text-lg lg:text-xl">
+                89
+              </h1>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
