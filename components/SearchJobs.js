6b9bce0f3ca99455c3f19552a3cf088ebc1907ce
yafet-123 <yafetaddisu123@gmail.com>
2022-10-27 02:08:54 +0300

    editing latest jobs

---
+++ components/SearchJobs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function SearchJobs() {
+  return (
+    <div className="flex flex-col w-full h-screen py-20 px-32">
+      <h1 className="font-light text-2xl md:text-3xl lg:text-4xl capitalize mb-5">
+        Search and Find Jobs in Ethiopia
+      </h1>
+      <hr className="w-full bg-gray-200 mb-5" />
+      <div className="w-full h-[55rem] border rounded-xl border-slate-800 shadow-2xl shadow-sky-200"></div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import { Hero } from "../components/Hero";
 import { LatestJobs } from "../components/LatestJobs";
 import { Navbar } from "../components/Navbar";
 import { News } from "../components/News";
+import { SearchJobs } from "../components/SearchJobs";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
       <Hero />
+      <SearchJobs />
       <LatestJobs />
       <News />
       <Advice />
