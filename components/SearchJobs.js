4cb2b087a5a354c6a8fbd396efbb64ce143c26af
yafet-123 <yafetaddisu123@gmail.com>
2022-10-27 14:24:29 +0300

    editing search jobs

--- components/SearchJobs.js
+++ components/SearchJobs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { LatestJobsList } from "../data/LatestJobs";
 import Link from "next/link";
+import { BiCategory } from "react-icons/bi";
+import { CiLocationOn } from "react-icons/ci";
 export function SearchJobs() {
   return (
     <div className="flex flex-col w-full h-screen py-20 px-7 md:px-32">
@@ -9,8 +11,8 @@ export function SearchJobs() {
       </h1>
       <hr className="w-full bg-gray-200 mb-5" />
       <div className="flex w-full h-[55rem]">
-        <div className="flex-1 px-5 border rounded-xl shadow-2xl shadow-sky-200 p-3">
-          <div className="w-full h-full border-slate-800 overflow-y-scroll">
+        <div className="flex-1 px-5 border rounded-xl shadow-2xl shadow-sky-200 border-slate-300  p-3">
+          <div className="w-full h-full overflow-y-scroll">
             <div className="grid md:grid-cols-3 gap-5 px-20 py-10">
               {LatestJobsList.map((data, index) => (
                 <div
@@ -29,13 +31,15 @@ export function SearchJobs() {
           </div>
         </div>
 
-        <ul className="flex flex-col items-center mt-5">
-          <li className="py-3 px-6 bg-gray-200 border-2 border-black w-full">
-            Jobs by Category
-          </li>
-          <li className="py-3 px-6 bg-gray-200 border-2 border-black w-full">
-            Jobs by Location
-          </li>
+        <ul className="mt-5">
+          <a className="py-3 px-6 bg-gray-200 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400">
+            <BiCategory size={20} />
+            <span className="text-xl ml-3">Jobs by Category</span>
+          </a>
+          <a className="py-3 px-6 bg-gray-200 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400">
+            <CiLocationOn size={20} />
+            <span className="text-xl ml-3">Jobs by Location</span>
+          </a>
         </ul>
       </div>
     </div>
