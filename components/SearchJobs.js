0e2e626398d0d5f23b29ad085be706774162faa6
yafet-123 <yafetaddisu123@gmail.com>
2022-10-28 03:25:31 +0300

    editing search jobs

--- components/SearchJobs.js
+++ components/SearchJobs.js
@@ -4,40 +4,63 @@ import Link from "next/link";
 import { BiCategory } from "react-icons/bi";
 import { GoLocation } from "react-icons/go";
 import { useState } from "react";
+import { JobsByLocation } from "../data/JobsByLocation";
 export function SearchJobs() {
-  const [jobs, setJobs] = useState("");
+  const [jobs, setJobs] = useState("category");
   console.log(jobs);
   return (
-    <div className="flex flex-col w-full h-screen py-20 px-7 md:px-32">
+    <div className="flex flex-col w-full h-full py-20 px-7 md:px-32">
       <h1 className="font-light text-2xl md:text-3xl lg:text-4xl capitalize mb-5 text-center md:text-left">
         Search and Find Jobs in Ethiopia
       </h1>
       <hr className="w-full bg-gray-200 mb-5" />
-      <div className="flex w-full h-full lg:h-[55rem]">
+      <div className="flex w-full h-full lg:h-[45rem]">
         <div className="flex-1 px-5 border rounded-xl shadow-2xl shadow-sky-200 border-slate-300  p-3">
           <div className="w-full h-full overflow-y-scroll">
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 px-20 py-10">
-              {LatestJobsList.map((data, index) => (
-                <div
-                  className="flex justify-between items-center mb-3 group hover:bg-gray-200 px-4 py-2"
-                  key={index}
-                >
-                  <h1 className="font-normal text-sm md:text-lg lg:text-xl capitalize group-hover:text-orange-500">
-                    Accounting finance
-                  </h1>
-                  <h1 className="px-5 py-2 border rounded-xl border-gray-200 text-blue-800 font-bold text-sm md:text-lg lg:text-xl group-hover:text-orange-500 group-hover:border-orange-200">
-                    89
-                  </h1>
-                </div>
-              ))}
-            </div>
+            {jobs == "category" ? (
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 px-20 py-10">
+                {LatestJobsList.map((data, index) => (
+                  <div
+                    className="flex justify-between items-center mb-3 group hover:bg-gray-200 px-4 py-2"
+                    key={index}
+                  >
+                    <h1 className="font-normal text-sm md:text-lg lg:text-xl capitalize group-hover:text-orange-500">
+                      Accounting finance
+                    </h1>
+                    <h1 className="px-5 py-2 border rounded-xl border-gray-200 text-blue-800 font-bold text-sm md:text-lg lg:text-xl group-hover:text-orange-500 group-hover:border-orange-200">
+                      89
+                    </h1>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 px-20 py-10">
+                {JobsByLocation.map((data, index) => (
+                  <div
+                    className="flex justify-between items-center mb-3 group hover:bg-gray-200 px-4 py-2"
+                    key={index}
+                  >
+                    <h1 className="font-normal text-sm md:text-lg lg:text-xl capitalize group-hover:text-orange-500">
+                      jobs in {data.location}
+                    </h1>
+                    <h1 className="px-5 py-2 border rounded-xl border-gray-200 text-blue-800 font-bold text-sm md:text-lg lg:text-xl group-hover:text-orange-500 group-hover:border-orange-200">
+                      {data.howmany}
+                    </h1>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
 
         <ul className="mt-5">
           <li
             onClick={(e) => setJobs("category")}
-            className="py-3 px-6 bg-gray-200 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400"
+            className={
+              jobs == "category"
+                ? "py-3 bg-white px-6 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400"
+                : "py-3 bg-gray-200 px-6 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400"
+            }
           >
             <BiCategory size={20} />
             <span className="hidden lg:inline-flex text-xl ml-3">
@@ -46,7 +69,11 @@ export function SearchJobs() {
           </li>
           <li
             onClick={(e) => setJobs("location")}
-            className="py-3 px-6 bg-gray-200 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400"
+            className={
+              jobs == "location"
+                ? "py-3 bg-white px-6 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400"
+                : "py-3 bg-gray-200 px-6 border border-slate-300 flex items-center hover:bg-white hover:text-blue-400"
+            }
           >
             <GoLocation size={20} />
             <span className="hidden lg:inline-flex text-xl ml-3">
---
+++ data/JobsByLocation.js
@@ -0,0 +1,64 @@
+export const JobsByLocation = [
+  {
+    location: "Addis Ababa",
+    howmany: 315,
+  },
+  {
+    location: "Afar",
+    howmany: 14,
+  },
+
+  {
+    location: "Amhara",
+    howmany: 72,
+  },
+
+  {
+    location: "Benishangul Gumuz",
+    howmany: 4,
+  },
+
+  {
+    location: "Dire Dewa",
+    howmany: 20,
+  },
+
+  {
+    location: "Gambela",
+    howmany: 5,
+  },
+
+  {
+    location: "Harari",
+    howmany: 7,
+  },
+  {
+    location: "Oromiya",
+    howmany: 90,
+  },
+
+  {
+    location: "Sidama",
+    howmany: 56,
+  },
+
+  {
+    location: "Somali",
+    howmany: 34,
+  },
+
+  {
+    location: "SNNPR",
+    howmany: 45,
+  },
+
+  {
+    location: "Tigray",
+    howmany: 86,
+  },
+
+  {
+    location: "South West Ethopian Region",
+    howmany: 50,
+  },
+];
