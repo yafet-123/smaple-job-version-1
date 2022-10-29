511776c8a5dbc74353c16a2f8c6d56bd1a8fd412
yafet-123 <yafetaddisu123@gmail.com>
2022-10-29 04:05:19 +0300

    editing footer components

--- components/Footer.js
+++ components/Footer.js
@@ -28,8 +28,8 @@ export function Footer() {
   ];
 
   return (
-    <footer className="bg-gray-300">
-      <div className="flex flex-col lg:flex-row justify-between p-10 bg-white">
+    <footer className="bg-blue-400">
+      <div className="flex flex-col lg:flex-row justify-between p-10 bg-blue-500">
         <div className="flex flex-col items-center justify-between space-y-5 md:items-start w-full md:w-1/4 mb-10 md:mb-0">
           <div className="">
             <Link href="/">
@@ -45,7 +45,7 @@ export function Footer() {
             </Link>
           </div>
 
-          <p className="text-copyrightTextColor w-3/4">
+          <p className="text-xl text-white w-3/4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elitadipisicing
             elit.
           </p>
@@ -53,64 +53,54 @@ export function Footer() {
             {socialMediaLinks.map((paths, index) => {
               return (
                 <Link key={index} href="#">
-                  <a>{paths.path}</a>
+                  <a className="text-white">{paths.path}</a>
                 </Link>
               );
             })}
           </div>
         </div>
         <div className="grid grid-cols-2 gap-6 lg:grid-cols-3 w-full lg:w-3/4">
-          <div className="flex flex-col space-y-2 text-black">
+          <div className="flex flex-col space-y-2 text-white">
             <h1 className="text-lg font-bold">Quick Links</h1>
             {quickLinks.map((links, index) => {
               return (
                 <Link key={index} href="#">
-                  <a className="text-copyrightTextColor hover:text-ecffSecondary">
-                    {links.link}
-                  </a>
+                  <a className="text-lg hover:text-orange-500">{links.link}</a>
                 </Link>
               );
             })}
           </div>
 
-          <div className="flex flex-col space-y-2 text-black">
-            <h1 className="text-lg font-bold">About Us</h1>
+          <div className="flex flex-col space-y-2 text-white">
+            <h1 className="text-xl font-bold">About Us</h1>
             {aboutUsLinks.map((links, index) => {
               return (
                 <Link key={index} href="#">
-                  <a className=" text-copyrightTextColor hover:text-ecffSecondary">
-                    {links.link}
-                  </a>
+                  <a className=" text-lg hover:text-orange-500">{links.link}</a>
                 </Link>
               );
             })}
           </div>
 
-          <div className="flex flex-col space-y-2 text-black float-right">
+          <div className="flex flex-col space-y-2 text-white float-right">
             <h1 className="text-xl font-bold">Contact Info</h1>
-            <p href="#" className="text-copyrightTextColor">
+            <p href="#" className="text-lg hover:text-orange-500">
               Jobs finder
             </p>
-            <p className="text-copyrightTextColor hover:text-ecffSecondary">
-              +251-934-73-10-38
-            </p>
-            <p className="text-copyrightTextColor hover:text-ecffSecondary">
-              +251-934-73-10-38
-            </p>
-            <Link href="mailto:info@ecff.org.et">
-              <a className="text-copyrightTextColor hover:text-ecffSecondary">
+            <p className="text-lg hover:text-orange-500">+251-934-73-10-38</p>
+            <p className="text-lg hover:text-orange-500">+251-934-73-10-38</p>
+            <Link href="yafetaddisu123@gmail.com">
+              <a className="text-lg hover:text-orange-500">
                 yafetaddisu123@gmail.com
               </a>
             </Link>
           </div>
         </div>
       </div>
-      <div className="flex justify-between items-center footerBackgroundColorDark h-8 mx-10">
-        <div className="text-copyrightTextColor md:block">&copy;Hulu 2022.</div>
+      <div className="flex justify-between items-center text-white h-8 mx-10">
+        <div className="text-lg md:block">&copy;Hulu 2022.</div>
 
-        <div className="text-copyrightTextColor md:block">
-          Powered by Hulu Media
-        </div>
+        <div className="text-lg md:block">Powered by Hulu Media</div>
       </div>
     </footer>
   );
